d64782a9f9aa0ebb97268b361d9d2d45b59208c0
blank <812860165@qq.com>
2018-05-27 20:11:40 +0800

    模拟登陆v0.0
    1使用爬虫模拟登陆哈尔滨工程大学教学一体化平台
    2.可以获得任意学期的成绩
    3..下一步提供计算每学期加权成绩
    4.下一步计算所有学期加权成绩

---
+++ 教务处.py
@@ -0,0 +1,81 @@
+import requests
+
+keyStr = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+
+def encodeInp(instr):
+    outstr = ""
+    chr1, chr2, chr3 = "","",""
+    enc1, enc2, enc3, enc4  = "","","",""
+    i = 0
+    while(i < len(instr)):
+        if i < len(instr):
+            chr1 = instr[i]; i+=1
+        if i < len(instr):
+            chr2 = instr[i]; i+=1
+        if i < len(instr):
+            chr3 = instr[i]; i+=1
+        enc1 = ord(chr1)>>2
+        if chr2 != "":
+            enc2 = ((ord(chr1) & 3) << 4) | (ord(chr2) >> 4)
+        else:
+            enc2 = (ord(chr1) & 3) << 4
+        if chr3 != "" and chr2 != "":
+            enc3 = ((ord(chr2) & 15) << 2) | (ord(chr3) >> 6)
+        elif chr2 != "":
+            enc3 = (ord(chr2) & 15) << 2
+        if chr3 != "":
+            enc4 = ord(chr3) & 63
+        if chr2 == "":
+            enc3 = 64
+            enc4 = 64
+        elif chr3 == "":
+            enc4  = 64
+        # print(enc1, enc2, enc3, enc4)
+        outstr = outstr + keyStr[enc1] + keyStr[enc2] + keyStr[enc3] + keyStr[enc4]
+        chr1, chr2, chr3 = "", "", ""
+        enc1, enc2, enc3, enc4 = "", "", "", ""
+    return outstr
+
+def getPassKey(username, password):
+    encodeInp_username = encodeInp("2014022125")
+    encodeInp_password = encodeInp("10012718")
+    passKey = encodeInp_username+"%%%"+encodeInp_password
+    print(passKey)
+    return passKey
+
+def getcontent(username = "2014022125", password = "10012718"):
+    headers = {
+        'User-Agent':
+            'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.61 Safari/537.36',
+        'Host':'edusys.hrbeu.edu.cn',
+        'Origin':'http://edusys.hrbeu.edu.cn'
+    }
+    url = 'http://edusys.hrbeu.edu.cn/jsxsd/xk/LoginToXk'
+    passKey = getPassKey(username, password)
+    data = {
+        'encoded':
+            passKey
+    }
+    session = requests.Session()
+    resp = session.post(url, data)
+    # print(resp.text)
+
+    # geturl = 'http://edusys.hrbeu.edu.cn/jsxsd/kscj/cjcx_query.do'
+    # resp = session.get(geturl)
+    # print(resp.text)
+
+    score_url = "http://edusys.hrbeu.edu.cn/jsxsd/kscj/cjcx_list"
+    #2015-1016第一学期:2015-1016-1
+    #2015-1016第二学期:2015-1016-2
+    #data内其余内容无需修改
+    data = {
+        'kksj':'2015-2016-1',
+        'kczx':'',
+        'kcmc':'',
+        'xsfs':'all',
+    }
+    resp = session.post(score_url, data)
+    print(resp.text)
+
+if __name__ == '__main__':
+    getcontent()
